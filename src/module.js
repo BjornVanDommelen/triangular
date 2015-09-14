fef663911975621f69379fc30a4adeac173394dc
bjornvandommelen <bjorn.van.dommelen@hinttech.com>
2015-09-14 14:10:38 +0200

    Added route for binaries

--- src/module.js
+++ src/module.js
@@ -7,6 +7,12 @@
     .config(['$locationProvider', useHtml5Urls]);
 
   function routeTridionPages($routeProvider) {
+    // Trigger a reload for image URLs as these need to come from the server
+    $routeProvider.when('/Images/:binaryFile', {
+      redirectTo: function(){ window.location.reload(); }
+    });
+
+    // Assume all other URLs are pages to be processed by the page controller
     $routeProvider.otherwise({
       template: '<ng-include src="pageView"></ng-include>',
       controller: 'PageController as pageController'
